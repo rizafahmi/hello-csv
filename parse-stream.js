b500dabba44cc2367e2a8a104010c6585a83b17f
Riza Fahmi <rizafahmi@gmail.com>
2016-02-19 10:26:47 +0700

    Finish stream feature

--- parse-stream.js
+++ parse-stream.js
@@ -1,3 +1,43 @@
-// 0. Please use readline (https://nodejs.org/api/readline.html) to deal with per line file reading
-// 1. Then use the parse API of csv-parse (http://csv.adaltas.com/parse/ find the Node.js Stream API section)
+'use strict';
 
+const debug = require('debug')('hello');
+
+var readline = require('readline');
+var Stream = require('stream');
+var fs = require('fs');
+
+const helper = require('./helper');
+
+var instream = fs.createReadStream('./sample.csv');
+var outstream = new Stream;
+
+var rl = readline.createInterface(instream, outstream);
+
+rl.on('line', function (line) {
+
+  debug(`sending data: ${line}`);
+
+  helper.sendSms(line, function afterSending(err, sendingStatus) {
+    var lineToLog;
+    if (err) {
+      debug(err.message);
+
+      lineToLog = {
+        sendingStatus,
+        line,
+      };
+    }
+
+    if (lineToLog) {
+      helper.logToS3(lineToLog, function afterLogging(err, loggingStatus) {
+        if (err) {
+          debug(err.message);
+        }
+      });
+    }
+  });
+});
+
+rl.on('close', function () {
+  console.log('Finished.');
+});
